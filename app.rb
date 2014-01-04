dab589e2d75f707a02ea55fad45a4c90eb17a45f
contessa <tessa.fallon@gmail.com>
2014-01-04 14:44:16 -0500

    add basic dev and cat pages

--- app.rb
+++ app.rb
@@ -5,4 +5,16 @@ class App < Sinatra::Application
   get '/' do
     erb :index
   end
+
+  get '/webarch' do
+  	erb :webarch
+  end
+
+  get '/dev' do
+  	erb :dev
+  end
+
+  get '/cat' do
+  	erb :cat
+  end
 end
